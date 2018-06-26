071f9436ccc9a4bb237ee2d76bbfadeb18181fcc
papazhora <4156464@gmail.com>
2018-06-26 16:30:31 +0300

    222

--- src/main/java/ru/testdb/Main.java
+++ src/main/java/ru/testdb/Main.java
@@ -1,9 +1,8 @@
 package ru.testdb;
 
 import com.mysql.cj.jdbc.Driver;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import java.sql.*;
 
 public class Main {
 
@@ -19,21 +18,62 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Connection connection;
-
         try {
             Driver driver = new Driver();
             DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Connection is OK!");
-            if (!connection.isClosed()) {
-                connection.close();
-                System.out.println("Connection closed!");
-            }
+        } catch (SQLException e) {
+            System.out.println("Driver Error!");
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement statement = connection.createStatement();
+            System.out.println("Connection OK!");
+                //statement.execute("insert into users (name, age, email) values('Alex', 33, 'ls@mail.ru');");
+                //statement.executeUpdate("UPDATE `users` SET `name`='aaaaa',`age`=100,`email`='wwww@wwww' WHERE name = 'ggg';");
+                //ResultSet res = statement.executeQuery("SELECT * FROM 'users' WHERE id=1");
+                //statement.addBatch("INSERT INTO users (name) values ('ffffrrrryyyyy')";
+                //statement.addBatch("INSERT INTO users (name, age, email) VALUES ('Big', 18, 'qwerty@mail.ru');");
+                //statement.addBatch("INSERT INTO users (name, email) VALUES ('Dic', 'dic@mail.ru');");
+                //statement.addBatch("INSERT INTO articles (auhtor, name_article) values ('ffff', 'yyyyyy';)");
+                //statement.addBatch("INSERT INTO articles (auhtor, name_article) values ('cccc', 'rttttr';)");
+                //statement.addBatch("INSERT INTO articles (auhtor, name_article) values ('vvvvvv', 'rhhhhr';)");
+                //statement.executeBatch();
+                statement.clearBatch(); // очищает Batch
+                System.out.println("Entry added!");
+                //System.out.println(res);
+                if (!connection.isClosed()) {
+                    connection.close();
+                    System.out.println("Connection closed!");
+
+
+                }
+                statement.getConnection(); // соединение с базой
+
+                 statement.close(); // закрыть соединение
 
         } catch (SQLException e) {
             System.out.println("Connection Error!");
         }
+
+
+
+
+//        Connection connection;
+//
+//        try {
+//            Driver driver = new Driver();
+//            DriverManager.registerDriver(driver);
+//            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+//            System.out.println("Connection is OK!");
+//            if (!connection.isClosed()) {
+//                connection.close();
+//                System.out.println("Connection closed!");
+//            }
+//
+//        } catch (SQLException e) {
+//            System.out.println("Connection Error!");
+//        }
 
     }
 }
